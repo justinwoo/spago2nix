bd0e10baf2d34fd0c2256b27800c9945e985088e
justinwoo <moomoowoo@gmail.com>
2019-06-17 11:41:02 +0200

    travis tests

--- default.nix
+++ default.nix
@@ -10,6 +10,13 @@ let
     inherit pkgs;
   };
 
+  easy-purescript-nix = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "e54b442633f59adedc3fbc81889ca7cb6b3ef087";
+    sha256 = "17i19dnqmd71bdz83bc3aak2rx3p1lvgwp1cc02w3sm9i24s9pvz";
+  });
+
 in pkgs.stdenv.mkDerivation {
   name = "spago2nix";
 
@@ -29,6 +36,7 @@ in pkgs.stdenv.mkDerivation {
     wrapProgram $target \
       --prefix PATH : ${pkgs.lib.makeBinPath [
         pkgs.nix-prefetch-git
+        easy-purescript-nix.spago
         easy-dhall-nix.dhall-json-simple
       ]}
   '';
