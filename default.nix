0706332bbadb0864fd1443d6087dcb58843f4e5c
Paul Young <paulyoungonline@gmail.com>
2019-06-27 18:47:53 +0100

    Revert "Use camel case"
    This reverts commit ea8730a6f8e31b214be9ab4a3317a58d5cfc1dbe.

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let easy-dhall-nix = pkgs: import (pkgs.fetchFromGitHub {
 }; in
 
 { pkgs ? import <nixpkgs> {}
-, dhallJson ? (easy-dhall-nix pkgs).dhall-json-simple
+, dhall-json ? (easy-dhall-nix pkgs).dhall-json-simple
 }:
 
 let easy-purescript-nix = import (pkgs.fetchFromGitHub {
@@ -39,7 +39,7 @@ pkgs.stdenv.mkDerivation {
         pkgs.nix-prefetch-git
         easy-purescript-nix.purs
         easy-purescript-nix.spago
-        dhallJson
+        dhall-json
       ]}
   '';
 }
