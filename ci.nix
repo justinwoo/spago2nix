96049fcec1fa4eff27005696f7af3af349263724
justinwoo <moomoowoo@gmail.com>
2019-10-17 09:42:09 +0300

    update purs

--- ci.nix
+++ ci.nix
@@ -1,12 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  easy-ps = import (pkgs.fetchFromGitHub {
-    owner = "justinwoo";
-    repo = "easy-purescript-nix";
-    rev = "50ebcb6107aec7562b8391e5b329c2856d79fc96";
-    sha256 = "1j7mrk094mvaadpsxcz11namrzng9pzn4yzzlzmlcn90q3jzma1v";
-  }) {
+  easy-ps = import (
+    pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "cc7196bff3fdb5957aabfe22c3fa88267047fe88";
+      sha256 = "1xfl7rnmmcm8qdlsfn3xjv91my6lirs5ysy01bmyblsl10y2z9iw";
+    }
+  ) {
     inherit pkgs;
   };
 
@@ -14,6 +16,7 @@ let
     inherit pkgs;
   };
 
-in pkgs.mkShell {
+in
+pkgs.mkShell {
   buildInputs = [ easy-ps.purs spago2nix ];
 }
