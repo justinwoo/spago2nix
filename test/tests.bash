4b9fd982e963ab4479fd3ce04b751a9005fcd859
Profpatsch <mail@profpatsch.de>
2020-12-31 14:47:45 +0100

    chore(nix): fix dependencies
    And a bit of shuffling to import the sources in the right way.
    
    default.nix should have the same interface than before, I make sure
    the existing args are still there.
    
    Originally created with niv, but removed all the bs.

--- test/tests.bash
+++ test/tests.bash
@@ -3,10 +3,18 @@
 
 set -e
 
-spago2nix
-spago2nix generate 8
-spago2nix install
-spago2nix build
-rm -rf output
-spago2nix build-nix
-nix-build build-project.nix
+function run () {
+  echo "$ $@"
+  "$@"
+}
+
+run spago2nix
+run spago2nix generate 8
+run spago2nix install \
+  --arg pkgs '(import ./deps.nix {}).pkgs'
+run spago2nix build \
+  --arg pkgs '(import ./deps.nix {}).pkgs'
+run rm -rf output
+run spago2nix build-nix \
+  --arg pkgs '(import ./deps.nix {}).pkgs'
+run nix-build build-project.nix
